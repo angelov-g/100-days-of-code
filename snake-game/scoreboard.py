a1aa58505ef827e4b61f8d830c4c998a6496522c
georgiangelov <georgi.angelov.2508@gmail.com>
2023-01-19 13:31:02 +0200

    added high-score tracking

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -15,13 +16,15 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
-
-    def increase_score(self):
         self.clear()
-        self.score += 1
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
