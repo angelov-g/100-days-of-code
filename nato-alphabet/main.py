072c507aa97a6e168feea1486372a8c8ff1b469d
georgiangelov <georgi.angelov.2508@gmail.com>
2023-01-26 17:36:30 +0200

    added exception handling to nato-alphabet project

--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -7,7 +7,26 @@ alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+while True:
+    user_input = input("Enter a word: ")
+    try:
+        nato_list = [alphabet_dict[letter.upper()] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(nato_list)
+        break
 
-user_input = input("Enter a word: ")
-nato_list = [alphabet_dict[letter.upper()] for letter in user_input]
-print(nato_list)
+
+# ANGELA'S RECURSION (Recursion)
+# def generate_phonetic():
+#     word = input("Enter a word: ").upper()
+#     try:
+#         output_list = [alphabet_dict[letter] for letter in user_input]
+#     except KeyError:
+#         print("Sorry, only letters in the alphabet please.")
+#         generate_phonetic()
+#     else:
+#         print(nato_list)
+#         
+# generate_phonetic()
