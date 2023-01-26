599b6ec31f367d93230ce8510c8c50f0936ed56b
georgiangelov <georgi.angelov.2508@gmail.com>
2023-01-26 14:33:49 +0200

    added Tkinter objects to the UI

--- password-manager/main.py
+++ password-manager/main.py
@@ -8,9 +8,34 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
 canvas = Canvas(width=200, height=200)
 lock_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_image)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_entry = Entry(width=38)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+username_entry = Entry(width=38)
+username_entry.grid(column=1, row=2, columnspan=2)
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
 
 window.mainloop()
