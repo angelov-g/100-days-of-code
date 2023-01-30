5c50b7e29d2a5c3f54918e88f6681aa6902ea64c
georgiangelov <georgi.angelov.2508@gmail.com>
2023-01-30 13:57:30 +0200

    imported datetime module

--- intermediate-plus/automated-birthday-wisher/main.py
+++ intermediate-plus/automated-birthday-wisher/main.py
@@ -1,13 +1,23 @@
-import smtplib
+# import smtplib
+#
+# my_email = "angelov.abw@gmail.com"
+# my_password = "dzpbztilkolmxqkw"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=my_password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs=my_email,
+#                         msg="Subject:Hello\n\n"
+#                             "This is the body of the email."
+#                         )
 
-my_email = "angelov.abw@gmail.com"
-my_password = "dzpbztilkolmxqkw"
+import datetime as dt
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs=my_email,
-                        msg="Subject:Hello\n\n"
-                            "This is the body of the email."
-                        )
+now = dt.datetime.now()
+year = now.year
+dow = now.weekday()
+print(dow)
+
+date_of_birth = dt.datetime(year=2005, month=1, day=30, hour=4, minute=20)
+print(date_of_birth)
