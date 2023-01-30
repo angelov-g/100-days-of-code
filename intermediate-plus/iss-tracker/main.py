c6cd1e29e517703abef25ce3f00e2ed0dd1189d7
angelov-g <104611160+angelov-g@users.noreply.github.com>
2023-01-30 22:14:14 +0200

    tidy up files

--- intermediate-plus/iss-tracker/main.py
+++ intermediate-plus/iss-tracker/main.py
@@ -1,33 +1,37 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 43.214050
-MY_LNG = 27.914734
+MY_LAT = 51.507351 # Your latitude
+MY_LONG = -0.127758 # Your longitude
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# data = response.json()
-#
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
-# position = (longitude, latitude)
-#
-# print(position)
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
 
 parameters = {
     "lat": MY_LAT,
-    "lng": MY_LNG,
-    "formatted": 0
+    "lng": MY_LONG,
+    "formatted": 0,
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
-
-print(sunrise)
-print(sunset)
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
 
-print(time_now.hour)
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+
+
