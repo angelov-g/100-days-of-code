54e0e5d6f20be5ad0cf032de1e882ed798878a2c
angelov-g <104611160+angelov-g@users.noreply.github.com>
2023-01-30 22:12:30 +0200

    sunrise and sunset time API

--- intermediate-plus/iss-tracker/main.py
+++ intermediate-plus/iss-tracker/main.py
@@ -1,10 +1,33 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 43.214050
+MY_LNG = 27.914734
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# position = (longitude, latitude)
+#
+# print(position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-position = (longitude, latitude)
+print(sunrise)
+print(sunset)
 
-print(position)
+time_now = datetime.now()
+
+print(time_now.hour)
