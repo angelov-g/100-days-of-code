9cc0224220abd34439681fb5eb9ac18b92b7d8fe
angelov-g <104611160+angelov-g@users.noreply.github.com>
2023-01-30 21:44:46 +0200

    basic requests to the ISS tracker API

--- intermediate-plus/iss-tracker/main.py
+++ intermediate-plus/iss-tracker/main.py
@@ -1,5 +1,10 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
+data = response.json()
 
-print(response)
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+position = (longitude, latitude)
+
+print(position)
