02bfe9531ca77659ee4dea6a5f2f486653e51894
georgiangelov <georgi.angelov.2508@gmail.com>
2023-01-30 10:10:51 +0200

    user interface setup

--- flash-card-app/main.py
+++ flash-card-app/main.py
@@ -1,2 +1,28 @@
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
 
+BACKGROUND_COLOR = "#B1DDC6"
+LANGUAGE_FONT = ("Ariel", 40, "italic")
+WORD_FONT = ("Ariel", 60, "bold")
+
+# -------------------- UI SETUP -------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+card_front = PhotoImage(file="images/card_front.png")
+card_back = PhotoImage(file="images/card_back.png")
+right_image = PhotoImage(file="images/right.png")
+wrong_image = PhotoImage(file="images/wrong.png")
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="Title", font=LANGUAGE_FONT)
+canvas.create_text(400, 263, text="word", font=WORD_FONT)
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_button = Button(image=wrong_image, highlightbackground=BACKGROUND_COLOR)
+wrong_button.grid(row=1, column=0)
+right_button = Button(image=right_image, highlightbackground=BACKGROUND_COLOR)
+right_button.grid(row=1, column=1)
+
+window.mainloop()
