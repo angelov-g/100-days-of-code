b96f7e78e58c4571fab541ec7ffde4ef6da29ac8
angelov-g <104611160+angelov-g@users.noreply.github.com>
2023-02-02 18:10:06 +0200

    question showing in UI

--- intermediate-plus/gui-quiz/ui.py
+++ intermediate-plus/gui-quiz/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 TEXT_FONT = ("Arial", 20, "italic")
@@ -6,7 +7,9 @@ TEXT_FONT = ("Arial", 20, "italic")
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,6 +19,7 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 125,
+                                                     width=280,
                                                      text="Some text here",
                                                      fill=THEME_COLOR,
                                                      font=TEXT_FONT)
@@ -29,4 +33,10 @@ class QuizInterface:
         self.false_button = Button(image=image_false, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
