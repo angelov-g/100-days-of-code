82cff424d5fc9a2de110201d5c9da3d4c676ab1b
angelov-g <104611160+angelov-g@users.noreply.github.com>
2023-02-02 18:20:33 +0200

    added user input through buttons

--- intermediate-plus/gui-quiz/ui.py
+++ intermediate-plus/gui-quiz/ui.py
@@ -26,11 +26,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         image_true = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=image_true, highlightthickness=0)
+        self.true_button = Button(image=image_true, highlightthickness=0, command=self.click_true)
         self.true_button.grid(row=2, column=0)
 
         image_false = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=image_false, highlightthickness=0)
+        self.false_button = Button(image=image_false, highlightthickness=0, command=self.click_false)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -40,3 +40,10 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def click_true(self):
+        self.quiz.check_answer("True")
+
+    def click_false(self):
+        self.quiz.check_answer("False")
+        
