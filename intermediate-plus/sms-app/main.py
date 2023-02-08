587683d3c5c6fd966c94e56d4eed4e53a6bf6d0d
georgiangelov <georgi.angelov.2508@gmail.com>
2023-02-08 09:25:27 +0200

    added alert if it will rain

--- intermediate-plus/sms-app/main.py
+++ intermediate-plus/sms-app/main.py
@@ -13,4 +13,15 @@ parameters = {
 response = requests.get(url=OWM_Endpoint, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data["list"][0]["weather"][0]["id"])
+weather_slice = weather_data["list"][:6]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+    
