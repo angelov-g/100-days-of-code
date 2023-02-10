8d30ff903dc5bbf9536a1d776ed4c794b8813dda
georgiangelov <georgi.angelov.2508@gmail.com>
2023-02-10 14:51:10 +0200

    check stock prices

--- intermediate-plus/stock-news-app/main.py
+++ intermediate-plus/stock-news-app/main.py
@@ -1,30 +1,50 @@
+import requests
+import datetime
+
+today = datetime.date.today()
+delta = datetime.timedelta(days=1)
+yesterday = today - delta
+before_yesterday = yesterday - delta
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+STOCK_API_KEY = "7C86ZIAJ6MWUOU8T"
+stock_params = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": STOCK,
+    "apikey": STOCK_API_KEY
+}
+
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_API_KEY = "bb989c705e85477bb64032e12cd404d0"
+news_params = {
+    "q": COMPANY_NAME,
+    "apikey": NEWS_API_KEY
+}
 
+stock_response = requests.get(url=STOCK_ENDPOINT, params=stock_params)
+stock_response.raise_for_status()
+stock_data = stock_response.json()
 
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
+yesterday_data = stock_data["Time Series (Daily)"][str(yesterday)]
+yesterday_closing_price = float(yesterday_data["4. close"])
 
+before_yesterday_data = stock_data["Time Series (Daily)"][str(before_yesterday)]
+before_yesterday_closing_price = float(before_yesterday_data["4. close"])
+difference = abs(yesterday_closing_price - before_yesterday_closing_price)
+diff_percent = (difference / yesterday_closing_price) * 100
 
+if diff_percent > 5:
+    print("Get News")
 
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
-
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
+# STEP 3: Use twilio.com/docs/sms/quickstart/python
 # Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
+# HINT 1: Consider using a List Comprehension.
 
 
-
-#Optional: Format the SMS message like this: 
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
