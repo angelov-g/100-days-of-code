79138c791839c57a383de51cd160c336105f0bc7
georgiangelov <georgi.angelov.2508@gmail.com>
2023-02-12 18:17:48 +0200

    make requests to nutritionix exercise api

--- intermediate-plus/workout-tracking/main.py
+++ intermediate-plus/workout-tracking/main.py
@@ -1,2 +1,24 @@
-APP_ID = "nutritionix APP ID"
-API_KEY = "nutritionix API KEY"
+import requests
+
+APP_ID = "APP ID"
+APP_KEY = "APP KEY"
+
+headers = {
+    "x-app-id" : APP_ID,
+    "x-app-key": APP_KEY
+}
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+performed_exercises = input("Tell me what exercises you did: ")
+
+exercise_config = {
+    "query": performed_exercises,
+    "gender": "male",
+    "weight_kg": 65,
+    "height_cm": 182,
+    "age": 24
+}
+
+response = requests.post(url=exercise_endpoint, json=exercise_config, headers=headers)
+print(response.text)
