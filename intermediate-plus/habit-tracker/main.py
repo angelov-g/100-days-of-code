760323d175e88df69492baddcee967b7d3348522
georgiangelov <georgi.angelov.2508@gmail.com>
2023-02-12 17:23:19 +0200

    add pixels to graph

--- intermediate-plus/habit-tracker/main.py
+++ intermediate-plus/habit-tracker/main.py
@@ -2,6 +2,7 @@ import requests
 
 TOKEN = "ajshd18u43j3rowad"
 USERNAME = "angelov-g"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -12,13 +13,14 @@ user_params = {
     "notMinor": "yes"
 }
 
+# CREATE USER
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Walking Graph",
     "unit": "Km",
     "type": "float",
@@ -29,7 +31,16 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
+# CREATE GRAPH available at https://pixe.la/v1/users/angelov-g/graphs/graph1
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
 
-# Graph available at https://pixe.la/v1/users/angelov-g/graphs/graph1
+add_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+add_pixel_config = {
+    "date": "20230212",
+    "quantity": "10"
+}
+
+response = requests.post(url=add_pixel_endpoint, json=add_pixel_config, headers=headers)
+print(response.text)
