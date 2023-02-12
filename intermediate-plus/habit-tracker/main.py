3a2ad49afafc72dececf1e97f66978c38bc024e0
georgiangelov <georgi.angelov.2508@gmail.com>
2023-02-12 17:09:03 +0200

    create graph for user

--- intermediate-plus/habit-tracker/main.py
+++ intermediate-plus/habit-tracker/main.py
@@ -1,13 +1,35 @@
 import requests
 
+TOKEN = "ajshd18u43j3rowad"
+USERNAME = "angelov-g"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "ajshd18u43j3rowad",
-    "username": "angelov-g",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Walking Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
+
+# Graph available at https://pixe.la/v1/users/angelov-g/graphs/graph1
