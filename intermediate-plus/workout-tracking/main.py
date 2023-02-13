e280287ef27c98a9df155fce259e4b351c95e6b2
georgiangelov <georgi.angelov.2508@gmail.com>
2023-02-13 09:18:16 +0200

    reorganized code for better readability

--- intermediate-plus/workout-tracking/main.py
+++ intermediate-plus/workout-tracking/main.py
@@ -5,23 +5,12 @@ from requests.auth import HTTPBasicAuth
 APP_ID = "APP ID"
 APP_KEY = "APP KEY"
 
-headers = {
-    "x-app-id": APP_ID,
-    "x-app-key": APP_KEY
-}
-
-basic = HTTPBasicAuth("angelov-g", "PASSWORD")
-
-# Format date and time
-today = datetime.now().strftime("%d/%m/%Y")
-time = (datetime.now().strftime("%H:%M:%S"))
-
-# User prompt
-performed_exercises = input("Tell me what exercises you did: ")
-
-# Request to the nutritionix APP
+# Endpoints
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/4ff6909e4841189431a21ec2b500b261/workoutTracking/workouts"
 
+# Request to Nutritionix
+performed_exercises = input("Tell me what exercises you did: ")
 exercise_config = {
     "query": performed_exercises,
     "gender": "male",
@@ -29,18 +18,26 @@ exercise_config = {
     "height_cm": 182,
     "age": 24
 }
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY
+}
 
 response = requests.post(url=exercise_endpoint, json=exercise_config, headers=headers)
 response_data = response.json()
 
-# Request to the sheety API
-sheety_endpoint = "https://api.sheety.co/4ff6909e4841189431a21ec2b500b261/workoutTracking/workouts"
+# Request to the sheety
+today_date = datetime.now().strftime("%d/%m/%Y")
+time_now = (datetime.now().strftime("%H:%M:%S"))
+
+# Basic authentication
+basic = HTTPBasicAuth("angelov-g", "PASSWORD")
 
 for exercise in response_data["exercises"]:
     sheety_config = {
         "workout": {
-            "date": today,
-            "time": time,
+            "date": today_date,
+            "time": time_now,
             "exercise": exercise["name"].title(),
             "duration": exercise["duration_min"],
             "calories": exercise["nf_calories"]
