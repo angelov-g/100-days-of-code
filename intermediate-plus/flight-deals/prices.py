c7fe8815a816137a1494a35fa4e7521fc4f291e3
georgiangelov <georgi.angelov.2508@gmail.com>
2023-02-14 15:28:40 +0200

    uncommented some lines

--- intermediate-plus/flight-deals/prices.py
+++ intermediate-plus/flight-deals/prices.py
@@ -1,14 +1,14 @@
 # This file is only used to represent the sheet data
 
 prices = [
-    # {"city": "Paris", "iataCode": "", "lowestPrice": 54},
-    # {"city": "Berlin", "iataCode": "", "lowestPrice": 42},
-    # {"city": "Tokyo", "iataCode": "", "lowestPrice": 485},
-    # {"city": "Sydney", "iataCode": "", "lowestPrice": 551},
-    # {"city": "Istanbul", "iataCode": "", "lowestPrice": 95},
-    # {"city": "Kuala Lumpur", "iataCode": "", "lowestPrice": 414},
-    # {"city": "New York", "iataCode": "", "lowestPrice": 240},
-    # {"city": "San Francisco", "iataCode": "", "lowestPrice": 260},
-    # {"city": "Cape Town", "iataCode": "", "lowestPrice": 378},
+    {"city": "Paris", "iataCode": "", "lowestPrice": 54},
+    {"city": "Berlin", "iataCode": "", "lowestPrice": 42},
+    {"city": "Tokyo", "iataCode": "", "lowestPrice": 485},
+    {"city": "Sydney", "iataCode": "", "lowestPrice": 551},
+    {"city": "Istanbul", "iataCode": "", "lowestPrice": 95},
+    {"city": "Kuala Lumpur", "iataCode": "", "lowestPrice": 414},
+    {"city": "New York", "iataCode": "", "lowestPrice": 240},
+    {"city": "San Francisco", "iataCode": "", "lowestPrice": 260},
+    {"city": "Cape Town", "iataCode": "", "lowestPrice": 378},
     {"city": "Bali", "iataCode": "DPS", "lowestPrice": 501}
 ]
