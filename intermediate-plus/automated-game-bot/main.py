ae142dbc0e5957d9cf70e4580488ffde733b99ec
georgiangelov <georgi.angelov.2508@gmail.com>
2023-03-01 20:49:21 +0200

    open webpages with selenium

--- intermediate-plus/automated-game-bot/main.py
+++ intermediate-plus/automated-game-bot/main.py
@@ -1,3 +1,7 @@
-import selenium_driver
+from selenium import webdriver
 
-chrome_driver_path = "/selenium_driver/chromedriver"
+# chrome_driver_path = "/selenium_driver/chromedriver"
+driver = webdriver.Chrome()
+
+driver.get("https://www.amazon.com")
+
