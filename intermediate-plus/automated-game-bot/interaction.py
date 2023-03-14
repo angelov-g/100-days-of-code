f2b67d981276f44f86e8a3c1fb0a9d4e028f3867
georgiangelov <georgi.angelov.2508@gmail.com>
2023-03-14 11:02:19 +0200

    finished interaction with wiki

--- intermediate-plus/automated-game-bot/interaction.py
+++ intermediate-plus/automated-game-bot/interaction.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 driver = webdriver.Chrome()
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
-print(article_count.text)
+# article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+# print(article_count.text)
+
+# content_portals = driver.find_element(By.LINK_TEXT, "Content portals")
+# content_portals.click()
+
+search_bar = driver.find_element(By.NAME, "search")
+search_bar.send_keys("Python")
+search_bar = driver.find_element(By.NAME, "search")
+search_bar.send_keys(Keys.RETURN)
+
+
+sleep(2)
+
