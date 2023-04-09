8408b4fa5da021f846e4e609a22d4485447dadc4
georgiangelov <georgi.angelov.2508@gmail.com>
2023-04-09 18:22:58 +0300

    finished sf-research capstone project

--- intermediate-plus/sf-rent-research/main.py
+++ intermediate-plus/sf-rent-research/main.py
@@ -1,5 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
 ZILLOW_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22" \
              "usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%" \
@@ -46,3 +54,19 @@ for apartment in apartments:
         prices.append(apartment.span.getText().split("+")[0])
         # Not all links include the domain
         links.append("https://www.zillow.com" + (apartment.a.get("href").split("https://www.zillow.com"))[-1])
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(FORM_URL)
+
+sleep(8)
+
+for i in range(len(addresses)):
+    input_fields = driver.find_elements(By.CSS_SELECTOR, ".Qr7Oae input")
+    submit = driver.find_element(By.CSS_SELECTOR, ".Fxmcue span")
+    input_fields[0].send_keys(addresses[i])
+    input_fields[1].send_keys(prices[i])
+    input_fields[2].send_keys(links[i])
+    submit.click()
+    submit_again = driver.find_element(By.CSS_SELECTOR, "a")
+    submit_again.click()
+    sleep(2)
