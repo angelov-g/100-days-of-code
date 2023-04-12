02440e85eec412f959ba78720b7749859fb044a9
georgiangelov <georgi.angelov.2508@gmail.com>
2023-04-12 15:37:35 +0300

    styling HTML with decorators

--- intermediate-plus/flask-intro/hello_flask.py
+++ intermediate-plus/flask-intro/hello_flask.py
@@ -2,6 +2,27 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper_function():
+        return f"<b>{function()}</b>"
+
+    return wrapper_function
+
+
+def make_emphasis(function):
+    def wrapper_function():
+        return f"<em>{function()}</em>"
+
+    return wrapper_function
+
+
+def make_underlined(function):
+    def wrapper_function():
+        return f"<u>{function()}</u>"
+
+    return wrapper_function
+
+
 @app.route('/')
 def hello_world():
     return '<h1 style="text-align:center">Hello, World!</h1>' \
@@ -10,6 +31,9 @@ def hello_world():
 
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def bye():
     return "Bye"
 
