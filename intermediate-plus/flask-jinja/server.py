212621ecaee552fff9fa8a9df5cfe0e52f8deef7
georgiangelov <georgi.angelov.2508@gmail.com>
2023-04-14 15:48:31 +0300

    Jinja templating with APIs

--- intermediate-plus/flask-jinja/server.py
+++ intermediate-plus/flask-jinja/server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
+
+GENDERIZE_ENDPOINT = "https://api.genderize.io"
+AGIFY_ENDPOINT = "https://api.agify.io"
 
 app = Flask(__name__)
 
@@ -10,6 +14,15 @@ def home():
     random_number = random.randint(1, 10)
     current_year = datetime.date.today().year
     return render_template("index.html", num=random_number, year=current_year)
+
+
+@app.route("/guess/<name_input>")
+def guess(name_input):
+    genderize_response = requests.get(GENDERIZE_ENDPOINT, params={"name": name_input})
+    gender_guess = genderize_response.json()["gender"]
+    agify_response = requests.get(AGIFY_ENDPOINT, params={"name": name_input})
+    age_guess = agify_response.json()["age"]
+    return render_template("guess.html", name=name_input.title(), gender=gender_guess, age=age_guess)
 
 
 if __name__ == "__main__":
