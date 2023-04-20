1eb56452ab4ef9ae30237c898e7db5c0a362d307
georgiangelov <georgi.angelov.2508@gmail.com>
2023-04-20 10:42:17 +0300

    finished bootstrap blog project

--- advanced/blog-bootstrap/server.py
+++ advanced/blog-bootstrap/server.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from post import Post
+import requests
 
 app = Flask(__name__)
+
+POSTS_ENDPOINT = "https://api.npoint.io/07c01eb3e07ce1edc506"
+response = requests.get(POSTS_ENDPOINT)
+all_posts = response.json()
+
+posts_list = []
+for post in all_posts:
+    post_object = Post(post["id"], post["title"], post["subtitle"], post["body"], post["date"], post["image_url"])
+    posts_list.append(post_object)
 
 
 @app.route("/")
 def get_home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/about")
@@ -16,6 +27,15 @@ def get_about():
 @app.route("/contact")
 def get_contact():
     return render_template("contact.html")
+
+
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    requested_post = None
+    for post_iter in posts_list:
+        if post_iter.id == post_id:
+            requested_post = post_iter
+    return render_template("post.html", requested_post=requested_post)
 
 
 if __name__ == "__main__":
