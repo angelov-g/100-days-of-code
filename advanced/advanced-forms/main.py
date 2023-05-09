f2eb5971211896b71e8b109ed25e62b32472c03a
georgiangelov <georgi.angelov.2508@gmail.com>
2023-05-09 14:20:26 +0300

    retrieving data from forms

--- advanced/advanced-forms/main.py
+++ advanced/advanced-forms/main.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class MyForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired(), Email()])
-    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    email = StringField(label="Email", validators=[DataRequired(), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 
@@ -16,15 +16,19 @@ app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = MyForm()
-    login_form.validate_on_submit()
-    return render_template('login.html', form=login_form)
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
