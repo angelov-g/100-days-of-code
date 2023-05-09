0d87cf8a96ba1d681cfc96098c75a6ef084b473e
georgiangelov <georgi.angelov.2508@gmail.com>
2023-05-09 14:15:07 +0300

    added validators

--- advanced/advanced-forms/main.py
+++ advanced/advanced-forms/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class MyForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 
@@ -18,9 +19,10 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = MyForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
